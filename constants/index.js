24c52693b56c2a04ed4b1c313ad7f447641cd37f
Thinh Nguyen <thinhnp@ymese.com>
2021-11-09 15:30:20 +0700

    Remove debug log

--- constants/index.js
+++ constants/index.js
@@ -1,8 +1,6 @@
 const MOTE_RATE = 1000000000;
 const TESTNET_RPC_URL = process.env.TESTNET_RPC_URL || 'http://159.65.118.250:7777/rpc';
 
-console.log('Connecting to PPC_URL', TESTNET_RPC_URL, process.env);
-
 const ERC20_TOKEN_ATTRS = ['name', 'symbol', 'total_supply', 'decimals'];
 
 module.exports = {
