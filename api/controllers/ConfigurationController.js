64d0ff99b6be254d5140ed72730f4858f7863b97
Thinh Nguyen <thinhnp@ymese.com>
2021-11-09 14:26:08 +0700

    Add api version

--- api/controllers/ConfigurationController.js
+++ api/controllers/ConfigurationController.js
@@ -2,6 +2,6 @@ const { MOTE_RATE, TESTNET_RPC_URL } = require('../../constants');
 
 module.exports = {
 	get: async (req, res) => {
-		res.json({ MOTE_RATE, TESTNET_RPC_URL });
+		res.json({ MOTE_RATE, TESTNET_RPC_URL, API_VERSION: '1.0.0' });
 	},
 };
