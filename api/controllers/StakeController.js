4eee43653ed82ce879052a906fc9fcb9f33f4256
Thinh Nguyen <thinhnp@ymese.com>
2021-11-12 19:42:37 +0700

    Retrive the eraId

--- api/controllers/StakeController.js
+++ api/controllers/StakeController.js
@@ -3,7 +3,8 @@ const { getValidators } = require('../services/ValidatorServices');
 module.exports = {
 	getValidators: async (req, res, next) => {
 		try {
-			const validators = await getValidators();
+			const { eraId } = req.params;
+			const validators = await getValidators(eraId);
 			res.json(validators);
 		} catch (err) {
 			next(err);
