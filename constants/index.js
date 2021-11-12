0b48162a0cb1c47b598b92ab95c0a87f0f13383e
kiennguyen <kiennguyen@niometrics.com>
2021-11-12 17:17:09 +0700

    Get value from dictionary

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 const MOTE_RATE = 1000000000;
-const TESTNET_RPC_URL = process.env.TESTNET_RPC_URL || 'http://159.65.118.250:7777/rpc';
+const TESTNET_RPC_URL = process.env.TESTNET_RPC_URL || 'http://95.179.179.85:7777/rpc';
 
 const ERC20_TOKEN_ATTRS = ['name', 'symbol', 'total_supply', 'decimals'];
 
