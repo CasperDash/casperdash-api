c38bea3f84dc61a1190b7881ea9518f4e1ee7e6e
kiennguyen <kiennguyen@niometrics.com>
2021-11-29 15:12:51 +0700

    Update license link

--- docs/basicInfo.js
+++ docs/basicInfo.js
@@ -11,7 +11,7 @@ module.exports = {
 		},
 		license: {
 			name: 'MIT',
-			url: 'https://opensource.org/licenses/MIT',
+			url: 'https://raw.githubusercontent.com/CasperDash/casperdash-client/develop/LICENSE',
 		},
 	},
 };
