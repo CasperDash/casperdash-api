4c631876d9c1ff70e15c828d7602fc019253eb1f
kiennguyen <kiennguyen@niometrics.com>
2021-12-09 17:52:44 +0700

    Update mainnet node config

--- config/mainnet/RPCConfig.js
+++ config/mainnet/RPCConfig.js
@@ -3,7 +3,6 @@
 const RPC_CONFIG = [
 	{ country: 'Singapore', ip: '134.209.110.11' },
 	{ country: 'Singapore', ip: '68.183.226.35' },
-	{ country: 'Vietnam', ip: '14.224.155.176' },
 ];
 
 module.exports = {
