4af85a858664a738362010019a0040b827f5ee8f
Thinh Nguyen <gaupoit@outlook.com>
2022-05-25 16:26:59 +0700

    Config testnet env for partnership validators testing

--- pm2.config.js
+++ pm2.config.js
@@ -10,6 +10,14 @@ module.exports = {
 			env_staging: {
 				NODE_ENV: 'testnet',
 				NFT_STORAGE_API_TOKEN: process.env.NFT_STORAGE_API_TOKEN,
+				PARTNERSHIP_VALIDATORS: [
+					{
+						public_key: '01028e248170a7f328bf7a04696d8f271a1debb54763e05e537eefc1cf24531bc7',
+						name: 'OriginStake - Only 1% fee until May 15 🔥🔥',
+						logo: 'https://originstake.com/.well-known/casper/originstake_logo_vali_256.png',
+						priority: 10,
+					},
+				],
 			},
 			interpreter: 'node@14.18.2', // Only need to set the node version here. NVM will automatically update node engine on server.
 		},
